28871c63fa568c4582229a171c7ecd6d25bd8fcb
jprodriguez <jprodriguez@uutech.es>
2024-11-11 22:27:09 +0100

    cleanup

--- source/movie_scraper.py
+++ source/movie_scraper.py
@@ -7,10 +7,19 @@
 
 
 import os
+import time
 import urllib
 import urllib.parse
+import urllib.request
 import urllib.robotparser
 from urllib.parse import urljoin
+import csv
+
+from typing import (
+    Tuple,
+    Union,
+    Set
+)
 
 import rich.progress
 from selenium import webdriver
@@ -23,18 +32,14 @@ from selenium.common.exceptions import *
 
 
 import rich
-from rich.progress import Progress, BarColumn, TextColumn
+from rich.progress import Progress
 
-from rich.table import Table
 from rich.pretty import pprint
 from bs4 import BeautifulSoup
-import time
-import requests
-import csv
-import whois
-import builtwith
 
-from tqdm import tqdm
+import requests
+from whois import whois
+from builtwith import builtwith
 
 
 from common import CSV_FN, console
@@ -48,35 +53,35 @@ TOTAL_MOVIES = 100
 
 def get_whois(url:str) -> dict:
     console.print("\n[bold underline]WhoIs[/]")
-    w = whois.whois(url)
+    w = whois(url)
     pprint(w, console=console)
     return w
 
 def get_builtwith(url:str) -> dict:
     console.print("\n[bold underline]BuiltWith[/]")
-    r = builtwith.builtwith(url)
+    r = builtwith(url)
     pprint(r, console=console)
     return r
 
-def parse_robots(url:str, user_agent:str) -> urllib.robotparser.RobotFileParser:
+def parse_robots(url:str, user_agent:str) -> Tuple[urllib.robotparser.RobotFileParser, float]:
     robot_url = urljoin(url, "robots.txt")
     rp = urllib.robotparser.RobotFileParser(robot_url)
-    
+
     # Leer el fichero robots.txt
     # Esta web rechaza la lectura realizada por defecto por la clase RobotFileParser,
     # así que leeremos el archivo con una request
     #rp.read()
-    
+
     headers={'User-Agent':user_agent,}
     request=urllib.request.Request(robot_url,None,headers)
     response = urllib.request.urlopen(request)
     robot_data_raw = response.read()
-    
+
     # Pasar el contenido a rp
     l = robot_data_raw.decode("utf-8").splitlines()
     rp.parse(l)
-    
-    
+
+
     # Obtener el delay que indica en el fichero robots
     crawl_delay = rp.crawl_delay(user_agent)
     if crawl_delay is None:
@@ -84,12 +89,12 @@ def parse_robots(url:str, user_agent:str) -> urllib.robotparser.RobotFileParser:
         crawl_delay = DEFAULT_CRAWL_DELAY_S
     else:
         # Establecer un mínimo de 1
-        crawl_delay = max(DEFAULT_CRAWL_DELAY_S, crawl_delay)
+        crawl_delay = max(DEFAULT_CRAWL_DELAY_S, float(crawl_delay))
 
     console.print("\n[bold underline]Robots[/]")
     pprint(l)
-    
-    return rp, crawl_delay
+
+    return (rp, crawl_delay)
 
 def get_web(user_agent:str, target_url:str) -> webdriver.Chrome:
     """Get a driver to the target web and click the cookies popup button
@@ -123,19 +128,18 @@ def get_web(user_agent:str, target_url:str) -> webdriver.Chrome:
         p_button = driver.find_element(By.CSS_SELECTOR, '.css-xlut8b')
         p_button.click()
         console.print("Privacy popup closed.")
-    except TimeoutException as te:
+    except TimeoutException:
         console.print("Privacy popup not found.")
-    except Exception as ex:
-        errorMsg = str(ex)
-        console.print_exception(errorMsg)
-    
+    except Exception:
+        console.print_exception()
+
     return driver
 
 def get_movie_links(user_agent:str,
                     driver:webdriver.Chrome, 
                     progress:rich.progress.Progress, 
                     rp: urllib.robotparser.RobotFileParser,
-                    crawl_delay:float) -> list[str]:
+                    crawl_delay:float) -> Set[str]:
     """Get the movie links from the main ranking page
 
     The algorithm will push the 'more' button until it gets
@@ -152,27 +156,27 @@ def get_movie_links(user_agent:str,
     Returns:
         list[str]: list of movies url
     """
-    
+
     # Add task to progress
     task = progress.add_task("Get movie links", total=TOTAL_MOVIES)
-    
+
     # Usamos un set para evitar duplicados en los enlaces
-    movie_links = set()
-    
+    movie_links: Set[str] = set()
+
     # Sólo se muestran los primeros 30 resultados, hay que volver a usar el webdriver de 
     # Selenium para cargar más resultados hasta que salgan las 100 películas 
     nb_empty_loops = 0
     while len(movie_links) < TOTAL_MOVIES:
         driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-        
+
         try:
             # https://www.browserstack.com/guide/element-click-intercepted-exception-selenium
-            
+
             moreResults_button = driver.find_element(By.CSS_SELECTOR, '.show-more')
-            
+
             driver.execute_script("arguments[0].scrollIntoView();", moreResults_button)
             WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.CSS_SELECTOR, '.show-more')))
-            
+
             actions = ActionChains(driver)
             actions.move_to_element(moreResults_button)
             actions.perform()
@@ -187,25 +191,26 @@ def get_movie_links(user_agent:str,
             console.print("No more results.")
             # Exit the loop
             break
-        
+
         # Extraemos los links de los resultados de la películas
         soup = BeautifulSoup(driver.page_source, 'html.parser')
         has_added_new_links = False
         for a_element in soup.select('.mc-title a'):
             # Check with robots if we can fetch the link
             href = a_element.get('href')
-            if rp.can_fetch(user_agent, href):
-                if len(movie_links) < TOTAL_MOVIES:
-                    if href not in movie_links:
-                        # This is a new link, add to the list
-                        movie_links.add(href)
-                        has_added_new_links = True
-                        # Update progress
-                        progress.advance(task)
-                else:
-                    break
-                
-                
+            if href is not None and isinstance(href, str):
+                if rp.can_fetch(user_agent, href):
+                    if len(movie_links) < TOTAL_MOVIES:
+                        if href not in movie_links:
+                            # This is a new link, add to the list
+                            movie_links.add(href)
+                            has_added_new_links = True
+                            # Update progress
+                            progress.advance(task)
+                    else:
+                        break
+
+
         if has_added_new_links is False:
             # There are no new links
             nb_empty_loops = nb_empty_loops + 1
@@ -215,13 +220,34 @@ def get_movie_links(user_agent:str,
         else:
             # Reset counter
             nb_empty_loops = 0
-            
+
     return movie_links
 
+def get_movie_field(soup:BeautifulSoup, name:str) -> str:
+    """Get a movie field
+
+    Args:
+        soup (BeautifulSoup): soup of the movie page
+        name (str): field name
+
+    Returns:
+        str: field text if found, else N/A
+    """
+    dt_element = soup.find("dt", string=name)
+    if dt_element is not None:
+        dd_element = dt_element.find_next("dd")
+        if dd_element is not None:
+            r = dd_element.get_text(strip=True)
+        else:
+            r = "N/A"
+    else:
+        r = "N/A"
+    return r
+
 def process_movie_links(user_agent:str,
-                        progress:rich.progress.Progress, 
+                        progress:rich.progress.Progress,
                         crawl_delay:float,
-                        movie_links: list[str]) -> list[list]:
+                        movie_links: Set[str]) -> list[list]:
     """Process the list of movie links to retrieve the data of each movie
 
     Args:
@@ -245,7 +271,7 @@ def process_movie_links(user_agent:str,
 
     # Extraemos los datos de las 100 películas usando requests porque no tienen ningún elemento complicado
     # y el popup de privacidad ya ha sido aceptado
-    
+
     task = progress.add_task("Process links", total=TOTAL_MOVIES)
     l = list(movie_links)
     n = len(l)
@@ -254,24 +280,39 @@ def process_movie_links(user_agent:str,
         movie_link = l[i]
         response = requests.get(movie_link, headers=headers, timeout=10)
         soup = BeautifulSoup(response.text, 'html.parser')
-        
+
         pos = i
-        tit = soup.select_one('#main-title > span:nth-child(1)').get_text(strip=True)
-        dur = soup.find("dt", string="Duración").find_next("dd").get_text(strip=True)
-        pais = soup.find("dt", string="País").find_next("dd").get_text(strip=True)
-        direc = soup.find("dt", string="Dirección").find_next("dd").get_text(strip=True)
-        gen = soup.find("dt", string="Género").find_next("dd").get_text(strip=True)
-        nota = soup.select_one('#movie-rat-avg').get_text(strip=True)
-        
+
+        # Title
+        title_element = soup.select_one('#main-title > span:nth-child(1)')
+        if title_element is not None:
+            tit = title_element.get_text(strip=True)
+        else:
+            tit = "N/A"
+
+        # Fields
+        dur = get_movie_field(soup, "Duración")
+        pais = get_movie_field(soup, "País")
+        direc = get_movie_field(soup, "Dirección")
+        gen = get_movie_field(soup, "Género")
+
+        # Rating
+        rating_element = soup.select_one('#movie-rat-avg')
+        if rating_element is not None:
+            nota = rating_element.get_text(strip=True)
+        else:
+            nota = "N/A"
+
+
         movies_data.append([pos,tit, dur, pais, direc, gen, nota])
-        
+
         # Esperamos entre consultas para reducir la presión sobre
         # el servidor y evitar ser bloqueados
         time.sleep(crawl_delay)
-    
+
         # Update progress
         progress.advance(task)
-    
+
     return movies_data
 
 
@@ -289,7 +330,7 @@ def store_dataset(progress:rich.progress.Progress, movies_data:list[list]):
 
 def movie_scraper():
 
-    
+
     # https://www.filmaffinity.com/es/ranking.php?rn=ranking_2024_topmovies
     base_url = "https://www.filmaffinity.com"
     target_url = f"{base_url}/es/ranking.php?rn=ranking_2024_topmovies"
@@ -301,7 +342,7 @@ def movie_scraper():
     user_agent = 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.0.7) Gecko/2009021910 Firefox/3.0.7'
     get_whois(base_url)
     get_builtwith(base_url)
-    rp, crawl_delay = parse_robots(base_url, user_agent)
+    (rp, crawl_delay) = parse_robots(base_url, user_agent)
 
     console.print()
     console.rule(f"[bold red]Process {target_url}")
